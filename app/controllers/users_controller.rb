2f8efa3fb7a38fdcf597c73a938d204ce8969a07
Felipe Sena <felipe.sena@mwmicroware.com.br>
2018-04-04 18:47:53 +0000

    created sessions[new, destroy and create], put some authenticate methods and log in, log out math buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  befor_action :set_user, only: [:edit, :update, :show]
+
   def index
     @user = User.paginate(page: params[:page], per_page: 5)
   end
@@ -20,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was update successfully"
       redirect_to articles_path
@@ -34,13 +35,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
 
+  def set_user
+        @user = User.find(params[:id])
   end
 end
